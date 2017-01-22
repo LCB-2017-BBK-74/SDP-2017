ecfb7a4ead17a7f752c3ed709f0cc037681d56a8
Lucie Burgess <lucie.burgess@icloud.com>
2017-01-22 00:19:21 +0000

    Took worksheet notes out of the comments in classes and added a ReadMe file within src folder.

--- Exercises_Java/Alarm_System/src/impl/ControlUnit.java
+++ Exercises_Java/Alarm_System/src/impl/ControlUnit.java
@@ -40,24 +40,4 @@ public class ControlUnit {
           }
       }
   }
-
-
 }
-
-/** Part 3. ControlUnit.pollSensors() method. Its current responsibilities are to add sensors to an ArrayList,
- * instantiating a new object each time new FireSensor(location) or new SmokeSensor(location) is called
- * If the sensor is triggered, get the sensor type and sensor location and print them;
- *  Finally to print a message stating that theu have been polled successfully
- *  Sensors will be polled each time the pollSensors() method is called but not always triggered
- *
- *  The current design - creating the list of sensors within the pollSensors() method in the ControlUnit class -
- *  violates the Single Responsibility Principle (the S of SOLID).
- *  Instead, we want to create the list outside the pollSensors() method, but still within the ControlUnit class.
- *  Alternatively we could create a new class just for creating the sensor list, but this feels over-complex.
- */
-
-/**
- * Part 4. Now I've created a constructor which initialises an empty arrayList, sensors, and configures the sensors
- * The configureSensors() method adds the sensors to a list
- * Now the pollSensors() method simply checks each sensor and prints out whether it has been triggered or polled
- */
