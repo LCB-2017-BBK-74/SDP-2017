42c94069c2c1c40c64ef0da3275ff233289f01db
Lucie Burgess <lucie.burgess@icloud.com>
2017-04-26 22:54:58 +0100

    Added Iterator but not quite working to the script

---
+++ Exercises_Scala/week_10/src/main/scala/iterator/ShapeIterator.scala
@@ -0,0 +1,28 @@
+package iterator
+
+/**
+  * Modified the Iterator
+  * This is not quite working but I really don't know why ...
+  * @param shapes
+  */
+
+class ShapeIterator(private var shapes: Array[Shape]) extends Iterator[Shape] {
+
+  private var index = -1
+
+  override def hasNext(): Boolean = {
+    index < shapes.length-1
+  }
+
+  override def next(): Shape = {
+    if (hasNext) {
+      val nextShape :Shape = shapes(index + 1)
+      index += 1
+      nextShape
+    }
+    else null
+  }
+
+  def remove(): Unit = shapes.filter(s => s.id != index)
+
+}
